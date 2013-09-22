6e38f36d7484693015a241e263f09fa41dd41928
Jim A Kinsman <jakinsman@gmail.com>
2013-09-21 23:17:35 -0500

    removed unneeded scripts

--- release/win32/clear_win32_release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm *.exe
-rm *.dll
--- release/win32/copy_win32_release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp ../../../build-JsRoboKey-Desktop_Qt_5_1_0_MinGW_32bit-Release/release/*.exe .
-cp ../../../build-JsRoboKey-Desktop_Qt_5_1_0_MinGW_32bit-Release/release/*.dll .
