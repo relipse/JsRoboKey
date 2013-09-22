bcbc057dd4bd9c0bdc190dca481b47d8c1c88813
Jim A Kinsman <jakinsman@gmail.com>
2013-09-21 19:11:19 -0500

    added some scripts to help with release

---
+++ release/win32/clear_win32_release.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm *.exe
+rm *.dll
---
+++ release/win32/copy_win32_release.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp ../../../build-JsRoboKey-Desktop_Qt_5_1_0_MinGW_32bit-Release/release/*.exe .
+cp ../../../build-JsRoboKey-Desktop_Qt_5_1_0_MinGW_32bit-Release/release/*.dll .
